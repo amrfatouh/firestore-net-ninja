cb82eba8ae91986d00275e8cad19810f529a1695
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-09-19 18:38:39 +0200

    realtime database

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const form = document.querySelector('#add-cafe-form');
+const cafeList = document.querySelector('#cafe-list');
+
+const addCafe = (doc) => {
+    let li = document.createElement('li');
+    li.innerHTML = `<span>${doc.data().name}</span>
+                    <span>${doc.data().city}</span>
+                    <div onclick='handleX.call(this)'>x</div>`;
+    li.setAttribute('data-id', doc.id)
+    cafeList.appendChild(li);
+}
+
+function handleX() {
+    let id = this.parentNode.dataset.id;
+    db.collection('cafes').doc(id).delete()
+}
 
 form.onsubmit = function (e) {
     e.preventDefault();
@@ -8,3 +23,17 @@ form.onsubmit = function (e) {
     });
     form.name.value = form.city.value = '';
 }
+
+db.collection('cafes').onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach(change => {
+        if (change.type === 'added') {
+            addCafe(change.doc);
+        } else if (change.type === 'modified') {
+            let li = document.querySelector(`li[data-id="${change.doc.id}"]`);
+            li.querySelector('span:first-of-type').textContent = change.doc.data().name;
+            li.querySelector('span:last-of-type').textContent = change.doc.data().city;
+        } else if (change.type === 'removed') {
+            document.querySelector(`li[data-id="${change.doc.id}"]`).remove();
+        }
+    })
+})
